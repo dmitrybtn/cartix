59c4d32d7a117b9f2df842498a551f5a466a86d2
Dmitry (Mac Mini) <tishurin@ya.ru>
2018-10-03 10:50:27 +0300

    Тесты

--- tests/acceptance/TestCest.php
+++ tests/acceptance/TestCest.php
@@ -12,16 +12,81 @@ class TestCest
 
     public function _before(\AcceptanceTester $I)
     {
+        Card::deleteAll();
+
         $I->haveFixtures([
             'users' => UsersFixture::className(),
         ]);
+    }
 
+    public function CardPlan(\AcceptanceTester $I)
+    {
+        $I->amOnPage(Url::to(['/users/login/login']));        
+        
+        $I->fillField('UserLogin[email]', 'root@root.ru');
+        $I->fillField('UserLogin[password]', '123');
+        $I->click('Войти');
+        $I->wait(0.5);        
+
+
+
+        $I->amOnPage(Url::to(['/cards/list/create']));  
+
+        // Создание техкарты
+        $I->fillField('Card[name]', 'New card');
+        $I->click('Сохранить');
+        $I->wait(0.5);
+        $I->seeRecord(Card::className(), ['name' => 'New card']);
+
+
+        // Создание остановки
+        $I->click('Добавить остановку', '.deskmenu-context'); // '//*[@id="w6"]/li[2]'
+        $I->fillField('CardTransfer[name]', 'New transfer');
+        $I->click('Сохранить');
+        $I->wait(0.5);
+        $I->seeRecord(CardTransfer::className(), ['name' => 'New transfer']);
+
+
+        // Создание объекта
+        $I->click('Добавить объект', '.cards_plan_transfers');
+        $I->fillField('CardObject[name]', 'New object');
+        $I->click('Сохранить', '.cards_plan_object--modal');
+        $I->wait(0.5);
+        $I->seeRecord(CardObject::className(), ['name' => 'New object']);
+
+        // Редактирование остановки
+        $I->click('New transfer', '.cards_plan_transfers');
+        $I->fillField('CardTransfer[name]', 'New 2 transfer');
+        $I->click('Сохранить');
+        $I->wait(0.5);
+        $I->seeRecord(CardTransfer::className(), ['name' => 'New 2 transfer']);
+
+
+        // Редактирование объекта
+        $I->click('New object', '.cards_plan_transfers');
+        $I->fillField('CardObject[name]', 'New 2 object');
+        $I->click('Сохранить', '.cards_plan_object--modal');
+        $I->wait(0.5);
+        $I->seeRecord(CardObject::className(), ['name' => 'New 2 object']);
+
+
+        // Удаление объекта
+        $I->click('New 2 object', '.cards_plan_transfers');
+        $I->click('Удалить объект', '.cards_plan_object--modal');
+        $I->acceptPopup();
+        $I->wait(0.5);
+        $I->dontSeeRecord(CardObject::className(), ['name' => 'New 2 object']);
+
+        // Удаление остановки
+        $I->click('New 2 transfer', '.cards_plan_transfers');
+        $I->click('Удалить остановку', '.cards_plan_transfer--modal');
+        $I->acceptPopup();
+        $I->wait(0.5);
+        $I->dontSeeRecord(CardTransfer::className(), ['name' => 'New 2 transfer']);
     }
 
     public function cardPlanMobile(\AcceptanceTester $I)
     {
-        Card::deleteAll();
-
         $I->resizeWindow(640, 1136);
 
         // Логин
@@ -63,78 +128,9 @@ class TestCest
         $I->click('Сохранить', '#card-object-form__controls-mobile');
         $I->wait(0.5);
         $I->seeRecord(CardObject::className(), ['name' => 'New object']);
-
-
-        $I->wait(5);
     }
 
-    public function _CardPlan(\AcceptanceTester $I)
-    {
-        Card::deleteAll();
 
-
-        $I->amOnPage(Url::to(['/users/login/login']));        
-        
-        $I->fillField('UserLogin[email]', 'root@root.ru');
-        $I->fillField('UserLogin[password]', '123');
-        $I->click('Войти');
-        $I->wait(0.5);        
-
-
-
-        $I->amOnPage(Url::to(['/cards/list/create']));  
-
-        // Создание техкарты
-        $I->fillField('Card[name]', 'New card');
-        $I->click('Сохранить');
-        $I->wait(0.5);
-        $I->seeRecord(Card::className(), ['name' => 'New card']);
-
-
-        // Создание остановки
-        $I->click('Добавить остановку', '.deskmenu-context'); // '//*[@id="w6"]/li[2]'
-        $I->fillField('CardTransfer[name]', 'New transfer');
-        $I->click('Сохранить');
-        $I->wait(0.5);
-        $I->seeRecord(CardTransfer::className(), ['name' => 'New transfer']);
-
-        // Создание объекта
-        $I->click('Добавить объект');
-        $I->fillField('CardObject[name]', 'New object');
-        $I->click('Сохранить', '.cards_plan_object--modal');
-        $I->wait(0.5);
-        $I->seeRecord(CardObject::className(), ['name' => 'New object']);
-
-        // Редактирование остановки
-        $I->click('New transfer', '.cards_plan_transfers');
-        $I->fillField('CardTransfer[name]', 'New 2 transfer');
-        $I->click('Сохранить');
-        $I->wait(0.5);
-        $I->seeRecord(CardTransfer::className(), ['name' => 'New 2 transfer']);
-
-
-        // Редактирование объекта
-        $I->click('New object', '.cards_plan_transfers');
-        $I->fillField('CardObject[name]', 'New 2 object');
-        $I->click('Сохранить', '.cards_plan_object--modal');
-        $I->wait(0.5);
-        $I->seeRecord(CardObject::className(), ['name' => 'New 2 object']);
-
-
-        // Удаление объекта
-        $I->click('New 2 object', '.cards_plan_transfers');
-        $I->click('Удалить объект', '.cards_plan_object--modal');
-        $I->acceptPopup();
-        $I->wait(0.5);
-        $I->dontSeeRecord(CardObject::className(), ['name' => 'New 2 object']);
-
-        // Удаление остановки
-        $I->click('New 2 transfer', '.cards_plan_transfers');
-        $I->click('Удалить остановку', '.cards_plan_transfer--modal');
-        $I->acceptPopup();
-        $I->wait(0.5);
-        $I->dontSeeRecord(CardTransfer::className(), ['name' => 'New 2 transfer']);
-    }
 
 
 
